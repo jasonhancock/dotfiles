e8a4a8e4786166f8165dbb22be81645ce9c448ca
Jason Hancock <jason@jasonhancock.com>
2013-11-13 21:03:06 -0800

    Leveling up my custom vagrant environment a bit.
    Passing through the project directory's name as an arg to my provisioning
    script (assumes Vagrantfile in top level of project directory). This is mainly
    so I can use that info in the bash prompt via the PS1 variable (I'm the guy
    with 15 terminal tabs open and I need some way to keep them straight and
    colorizing the prompt ususally works for me).
    
    Also adding multi-distro support (for now just Ubuntu/CentOS). Will extend
    this as necessary if I bring up any FreeBSD, Fedora, etc. Vagrant boxes.

--- .vagrant.d/bootstrap.sh
+++ .vagrant.d/bootstrap.sh
@@ -1,8 +1,43 @@
 #!/usr/bin/env bash
 
+if hash lsb_release 2>/dev/null; then
+    DISTRIB=`lsb_release -i | awk '{ print $3 }'`
+    RELEASE=`lsb_release -r | awk '{ print $2 }'`
+elif [[ "`uname`" == 'FreeBSD' ]]; then
+    DISTRIB='FreeBSD'
+    RELEASE=''
+else
+    echo "Unknown distro" 1>&2
+    DISTRIB="Unknown"
+    RELEASE="Unknown"
+fi
+
+case "$DISTRIB" in
+    Ubuntu)
+        export DEBIAN_FRONTEND=noninteractive
+        apt-get update > /dev/null
+        apt-get -y install vim
+        ;;
+    CentOS)
+        yum -y install vim-enhanced
+        ;;
+    *)
+        echo "Unknown disto $DISTRIB" 1>&2
+        ;;
+esac
+
 # TODO: update to support other distros
 export DEBIAN_FRONTEND=noninteractive
 apt-get update > /dev/null
 apt-get -y install vim
 
-wget -q -O /home/vagrant/.vimrc https://raw.github.com/jasonhancock/dotfiles/master/.vimrc
+su -c /home/vagrant/development/dotfiles/install.sh vagrant
+
+PROJECT=$1
+COLOR_FG=44
+COLOR_BG=70
+
+cat <<EOF > /home/vagrant/.bash_site
+export PS1='\033[${COLOR_FG}m\033[${COLOR_BG}m Vagrant - $PROJECT $DISTRIB $RELEASE \033[m\033[m \n[\u \w]\n\$ '
+EOF
+
