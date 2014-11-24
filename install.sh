9dedd31d5efa8f9fab2881d5bef6cc07b14fc435
Jason Hancock <jason@jasonhancock.com>
2014-11-24 09:59:38 -0800

    handle directories

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILES=".bash_profile .bashrc .gvimrc .vimrc .rpmmacros .vagrant.d/Vagrantfile .vagrant.d/bootstrap.sh"
+FILES=".bash_profile .bashrc .gvimrc .vimrc .rpmmacros .vagrant.d/Vagrantfile .vagrant.d/bootstrap.sh .composer/auth.json"
 
 cd ~
 
@@ -9,5 +9,13 @@ do
     if [ -f ~/$f ]; then
         rm $f
     fi
+
+    DIR=`dirname $f`
+    if [ "$DIR" != "." ]; then
+        if [ ! -d "$DIR" ]; then
+            mkdir -p ~/$DIR
+        fi
+    fi
+
     ln -s ~/development/dotfiles/$f ~/$f
 done
