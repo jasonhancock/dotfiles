2ddf6658aac37664ebb307c8b8d9c949e7ab2205
Jason Hancock <jason@jasonhancock.com>
2022-08-31 16:11:52 -0700

    terraform syntax highlighting

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -6,3 +6,4 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/rodjek/vim-puppet.git ~/.vim/bundle/vim-puppet
 git clone https://github.com/hashivim/vim-terraform.git ~/.vim/bundle/vim-terraform
 git clone https://github.com/posva/vim-vue.git ~/.vim/bundle/vim-vue
+git clone https://github.com/hashivim/vim-terraform.git ~/.vim/pack/plugins/start/vim-terraform
