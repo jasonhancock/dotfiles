ce10c40b668e6a727f2e14c2996073956ee19465
Jason Hancock <jason@jasonhancock.com>
2023-06-05 14:48:14 -0700

    add sqlite config, add .vim yaml indentation disable

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 UNAME=$(uname)
-FILES=".bash_profile .bashrc .gvimrc .vimrc .rpmmacros .vagrant.d/Vagrantfile .vagrant.d/bootstrap.sh .composer/auth.json .vim/spell/en.utf-8.add"
+FILES=".bash_profile .bashrc .gvimrc .vimrc .rpmmacros .vagrant.d/Vagrantfile .vagrant.d/bootstrap.sh .composer/auth.json .vim/spell/en.utf-8.add .vim/after/indent/yaml.vim .sqliterc"
 
 cd ~
 
